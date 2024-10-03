b7b7d90c99ed21102d009db4401ee89169aa2974
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-03 11:03:32 -0500

    Fetch API | Obtener las imágenes deseadas

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,20 @@
+const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=SjP3WHyFelRPbj5JidGdSXEfzYdTvgWZ&q=${category}&limit=10`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url
+    }));
+    console.log(gifs);
+    return gifs
+}
 
 export const GifGrid = ({ category }) => {
 
+    getGifs(category);
 
     return (
         <>
