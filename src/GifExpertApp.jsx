7ae673f67e6a4abfe23fc63bcf8bf78b0d7ff9b6
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-03 09:26:01 -0500

    Se esta desarrollando la logica de como nos gsutaria agregar nuevas categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        setCategories([...categories, 'HunterXHunter']);
+    }
+
     return (
         <>
+            {/* Título */}
             <h1>Gif Expert App</h1>
+
+            {/* Input */}
+            <AddCategory />
+
+            {/* Listado de Gifs */}
+            <button onClick={onAddCategory}>Add</button>
+            <ol>
+                {categories.map(category => (
+                    <li key={category}>{category}</li>
+                ))}
+                {/* <li>ABC</li> */}
+            </ol>
+            {/* Gif Item */}
         </>
-    )
+    );
 }
