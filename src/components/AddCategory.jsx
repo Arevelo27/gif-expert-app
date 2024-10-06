0c7046cc0ad6557953fe08924d2b4ccf33dad044
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-05 23:31:14 -0500

    Simular un submit del formulario, Jest Functions, se agrega un label al form  aria-label="form"

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -23,7 +23,7 @@ export const AddCategory = ({ onNewCategory }) => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} aria-label="form">
             <div>
                 <input
                     type="text"
