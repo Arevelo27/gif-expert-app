7ba1f5d3aba4acaed98150cd050595a4e096966d
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-10-06 22:17:03 -0500

    Se modifica logica de como se solcitan las imagenes

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -7,15 +7,15 @@ export const useFetchGifs = ( category ) => {
     const [images, setImages] = useState([]);
     const [isLoading, setIsLoading] = useState(true); // Agregar estado para isLoading
 
-    const getImages = async (category) => {
+    const getImages = async () => {
         const newImages = await getGifs(category);
         setImages(newImages);
         setIsLoading(false); // Cambiar el estado de isLoading a false después de cargar las imágenes
     }
 
     useEffect(() => {
-        getImages(category)
-    }, [category])
+        getImages()
+    }, [])
 
-    return [images, isLoading]; 
+    return {images, isLoading}; 
 }
